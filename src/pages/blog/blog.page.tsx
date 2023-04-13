15d3884f6023565944815fdb9a925e4a82051478
akbarsaputrait <akbarsaputrait@outlook.com>
2023-04-13 14:54:29 +0700

    Deploy

--- src/pages/blog/blog.page.tsx
+++
@@ -1,7 +0,0 @@
-import UnderDevelopment from '@/components/ui/under-development/under-development.component';
-
-const Blog: React.FC = () => {
-  return <UnderDevelopment />;
-};
-
-export default Blog;
